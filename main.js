bd6d6efe40d6769ccfec4843860bbfb907d53e56
loglot <88983354+loglot@users.noreply.github.com>
2023-08-27 15:26:46 -0500

    gedg

--- main.js
+++ main.js
@@ -2,13 +2,26 @@ const canvas = document.getElementById("game_screen");
 const ctx = canvas.getContext("2d");
 
 bean(800, 400, 50, 100) 
-bean(900, 400, 50, 100)
-bean(700, 400, 50, 100)
-bean(800, 600, 50, 100)
 // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 // i hate this
 
 function bean(x, y, width, height) {
+    ctx.beginPath();
+    ctx.rect(x - 5, y - 5, width + 10 , height - width / 2 + 10);
+    ctx.fillStyle = "#000000";
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.arc(x + (width / 2) - 5, y + (height - (width / 2)) - 5 , width / 2 + 10, 0, Math.PI * 2, false);
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.arc(x + (width / 2)- 5, y-5, width / 2 +10, 0, Math.PI * 2, false);
+    ctx.fill();
+    ctx.closePath();
+
     ctx.beginPath();
     ctx.rect(x, y, width, height - width / 2);
     ctx.fillStyle = "#afbfaf";
