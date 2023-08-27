a30803f379050ee49e308c97fe713797e282cd07
loglot <88983354+loglot@users.noreply.github.com>
2023-08-27 14:56:16 -0500

    fthtr

--- main.js
+++ main.js
@@ -10,13 +10,17 @@ bean(700, 400, 50, 100)
 function bean(x, y, width, height) {
     ctx.beginPath();
     ctx.rect(x, y, width, height - width / 2);
-    ctx.fillStyle = "#000000";
+    ctx.fillStyle = "#afbfaf";
     ctx.fill();
     ctx.closePath();
 
     ctx.beginPath();
     ctx.arc(x + (width / 2), y + (height - (width / 2)), width / 2, 0, Math.PI * 2, false);
-    ctx.fillStyle = "#afbfaf";
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.arc(x + (width / 2), y + (height + (width / 2)), width / 2, 0, Math.PI * 2, false);
     ctx.fill();
     ctx.closePath();
 }
