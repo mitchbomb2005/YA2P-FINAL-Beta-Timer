d3097a4dc889f1bc41e17c1662cac5d1416df133
loglot <88983354+loglot@users.noreply.github.com>
2023-08-27 14:21:14 -0500

    a

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
+const canvas = document.getElementById("game_screen");
+const ctx = canvas.getContext("2d");
 
+function bean(x, y, width, height) {
+    ctx.beginPath();
+    ctx.rect(0, 20, 1676, 878);
+    ctx.fillStyle = "#000000";
+    ctx.fill();
+    ctx.closePath();
 
-
-import { object } from './lib/var.js'
-
-
-
-console.log(object.a)
-object.a++
-console.log(object.a)
+    ctx.beginPath();
+    ctx.arc(800, 800, 50, 0, Math.PI * 2, false);
+    ctx.fillStyle = "#afbfaf";
+    ctx.fill();
+    ctx.closePath();
+}
