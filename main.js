94fa88ce604c7a35a700c7ea82a26bd4e48876bf
loglot <88983354+loglot@users.noreply.github.com>
2023-08-27 14:50:53 -0500

    bjdkrhf

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const ctx = canvas.getContext("2d");
 bean(800, 400, 50, 100) 
 
 // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-
+bean(900, 400, 50, 100)
+bean(700, 400, 50, 100)
 
 function bean(x, y, width, height) {
     ctx.beginPath();
