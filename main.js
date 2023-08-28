1c02cb637874c5edd47cc2992350d1a15353ddd1
loglot <88983354+loglot@users.noreply.github.com>
2023-08-28 07:00:53 -0500

    k876uk

--- main.js
+++ main.js
@@ -3,10 +3,26 @@ const ctx = canvas.getContext("2d");
 
 bean(800, 400, 50, 100) 
 bean(1000, 400, 100, 150)
-// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-// i hate this
 
 function bean(x, y, width, height) {
+
+    ctx.beginPath();
+    ctx.arc(x + (width / 2) - 5, y + 5, width / 2 +5, 0, Math.PI * 2, false);
+    ctx.fillStyle = "rgba(0, 0, 0, .1)";
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.arc(x + (width / 2) - 5, y+ (height - (width / 2)) + 5, width / 2 +5, 0, Math.PI * 2, false);
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.rect(x - 10, y, width + 10 , height - width / 2 + 10);
+    ctx.fill();
+    ctx.closePath();    
+
+
     ctx.beginPath();
     ctx.rect(x - 5, y - 5, width + 10 , height - width / 2 + 10);
     ctx.fillStyle = "#000000";
@@ -22,6 +38,8 @@ function bean(x, y, width, height) {
     ctx.arc(x + (width / 2), y, width / 2 +5, 0, Math.PI * 2, false);
     ctx.fill();
     ctx.closePath();
+
+//--------------------------------------------------------------------------------------------------------------------
 
     ctx.beginPath();
     ctx.rect(x, y, width, height - width / 2);
