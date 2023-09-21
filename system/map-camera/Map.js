c3985543eee52504bad52f625ea689beff36622c
loglot <88983354+loglot@users.noreply.github.com>
2023-09-21 07:07:00 -0500

    added a little to the map

--- system/map-camera/Map.js
+++ system/map-camera/Map.js
@@ -15,15 +15,13 @@ export class Map {
     }
 
     #createHitboxes() {
-        this.hitboxes[0] = new Hitbox(-10, 561, 1696, 1000);
+        this.hitboxes[0] = new Hitbox(-10, 561, 2096, 400);
         this.hitboxes[1] = new Hitbox(-10, 0, 1696, 300);
+        this.hitboxes[2] = new Hitbox(-10, 561, 1696, 300);
+        this.hitboxes[2] = new Hitbox(2085, -50, 100, 1000);
     }
 
     draw(cameraX, cameraY, color = "#0f0f0f") {
-
-        /*ctx.fillStyle = color;
-        ctx.fillRect(cameraX - 10, cameraY + 561, 1696, 1000);
-        ctx.fillRect(cameraX - 10, cameraY + 0, 1696, 300);*/
         
         for (let i = 0; i < this.hitboxes.length; i++) {
             this.hitboxes[i].draw(cameraX, cameraY);
