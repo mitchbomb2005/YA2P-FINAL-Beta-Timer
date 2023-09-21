929e8c048cfd3c8d9c1e5192c7331e26ae3e6fa3
loglot <88983354+loglot@users.noreply.github.com>
2023-09-20 20:50:18 -0500

    DID SO MUCH, MOVEMENT WORKS!!!
    Co-authored-by: ChillyTheEevee <ChillyTheEevee@users.noreply.github.com>

--- system/map-camera/Camera.js
+++ system/map-camera/Camera.js
@@ -1,17 +1,67 @@
+'use strict';
+
 export class Camera {
+
+    // System
+    keyManager;
+
+    // Variables
+    velX = 0;
+    velY = 0;
     x;
     y;
 
-    constructor(x, y) {
-        this.x = x
-        this.y = y
-        
+    // Constant
+    maxVelX = 100;
+    maxVelY = 100;
+
+    constructor(x, y, keyManager) {
+        this.keyManager = keyManager;
+        this.x = x;
+        this.y = y;
     }
 
-    move(x, y, frames = 60) {
-        for(let i = 0; i >= frames; i++)
-        return new Promise(resolve => setTimeout(resolve, ms));
-        this.x = this.x + (x / frames)
-        this.y = this.y + (y / frames)
+    update() {
+        this.#updateVelocity();
+        this.#move();
     }
+    
+    #updateVelocity() {
+        if (this.keyManager.isKeyPressed("KeyD")) {
+            this.velX -= 10;
+            if (Math.abs(this.velX) > this.maxVelX) {
+                velX = -maxVelX;
+            }
+
+        }
+        if (this.keyManager.isKeyPressed("KeyA")) {
+              this.velX += 10;
+              if (this.velX > this.maxVelX) {
+                velX = maxVelX;
+            }
+              
+        }
+        if (this.keyManager.isKeyPressed("KeyW")) {
+            this.velY += 10;
+            if (this.velY > this.maxVelY) {
+              velY = maxVelY;
+          }
+            
+        }
+        if (this.keyManager.isKeyPressed("KeyS")) {
+            this.velY -= 10;
+            if (Math.abs(this.velY) > this.maxVelY) {
+                velY = -maxVelY;
+            }
+
+        }
+    }
+
+    #move() {
+        this.x += this.velX;
+        this.y += this.velY;
+        this.velX = this.velX * .8
+        this.velY = this.velY * .8
+    }
+    
 }
--- system/map-camera/Map.js
+++ system/map-camera/Map.js
@@ -1,13 +1,13 @@
+'use strict';
+
 const canvas = document.getElementById("game_screen");
 const ctx = canvas.getContext("2d");
 
 export class Map {
 
-    draw(x, y,) {
+    draw(x, y, color = "#0f0f0f") {
 
-        ctx.fillStyle = "#0f0f0f";
-        //ctx.fillRect(x + 555, y + 561, 600, 150);
-        //ctx.fillRect(x + 555, y + 161, 600, 150);
+        ctx.fillStyle = color;
         ctx.fillRect(x - 10, y + 561, 1696, 1000);
         ctx.fillRect(x - 10, y + 0, 1696, 300);
         
