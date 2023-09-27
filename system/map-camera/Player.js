1e42eeb44c79b2aaf05c75aaad07631ada0a93bf
loglot <88983354+loglot@users.noreply.github.com>
2023-09-27 16:31:52 +0000

    made camera feel more like a drone

--- system/map-camera/Player.js
+++ system/map-camera/Player.js
@@ -41,8 +41,8 @@ export class Player {
             this.#move()
             this.#updateVelocity()
         }
-        this.camera.x = this.camera.x - (((this.camera.x /*+ 838*/) - this.x) / 5)
-        this.camera.y = this.camera.y - (((this.camera.y /*- 509*/) - this.y) / 5) 
+        this.camera.x = this.camera.x - (((this.camera.x /*+ 838*/) - this.x) / 10)
+        this.camera.y = this.camera.y - (((this.camera.y /*- 509*/) - this.y) / 10) 
 
     }
     
