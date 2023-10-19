96860f1a5de217e109bd8a5bb293fa6c1f730c75
loglot <88983354+loglot@users.noreply.github.com>
2023-10-19 11:53:12 +0000

    erased the map

--- system/map-player/map/mapCarryers/DeathMap.js
+++ system/map-player/map/mapCarryers/DeathMap.js
@@ -12,8 +12,6 @@ export class Death {
     }
     
     #createHitboxes() {
-        this.hitboxes[0] = new Hitbox(2792, 13, 2649, 4960, false)
-        this.hitboxes[1] = new Hitbox(3985, -2049, 1325, 202, false)
     }
 
     draw(cameraX, cameraY, color = "#500000") {
