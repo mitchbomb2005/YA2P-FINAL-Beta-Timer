7cd97b17ab903ebd3998973df9507a9ec4f12e0c
loglot <88983354+loglot@users.noreply.github.com>
2023-10-19 23:09:33 +0000

    changed warn.png

--- system/map-player/map/otherParts/Keys.js
+++ system/map-player/map/otherParts/Keys.js
@@ -6,16 +6,19 @@ export class Keys {
     keyD = new Image();
     keyW = new Image();
     keySpace = new Image();
-
+    warnLava = new Image();
     constructor(cam){
         this.camera = cam
         this.keyD.src = 'https://raw.githubusercontent.com/loglot/key-images/10212df9a78eeeb348d6302c3b79ac149670d2e6/Keyboard%20%26%20Mouse/Dark/D_Key_Dark.png';
         this.keyW.src = 'https://raw.githubusercontent.com/loglot/key-images/10212df9a78eeeb348d6302c3b79ac149670d2e6/Keyboard%20%26%20Mouse/Dark/W_Key_Dark.png';
         this.keySpace.src = 'https://raw.githubusercontent.com/loglot/key-images/main/Keyboard%20%26%20Mouse/Dark/Space_Key_Dark.png'
+        this.warnLava.src = 'https://raw.githubusercontent.com/loglot/yet-another-2d-platformer/main/other/images/warn.png'
     }
     drawKeys() {
         ctx.drawImage(this.keyD, 388 + this.camera.x, 600 + this.camera.y)
         this.jump(1788, 600)
+        ctx.drawImage(this.warnLava, 2778 + this.camera.x, 300 + this.camera.y)
+        ctx.drawImage(this.warnLava, 3478 + this.camera.x, 300 + this.camera.y)
     }
 
     jump(x, y) {
