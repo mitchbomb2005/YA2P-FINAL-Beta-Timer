f17c2fcd4c9bb35b7ddce091bb71a6bf01fa78f2
loglot <prattlog3@gmail.com>
2023-11-09 07:07:30 -0600

    added to the map

--- system/map-player/map/mapCarryers/DeathMap.js
+++ system/map-player/map/mapCarryers/DeathMap.js
@@ -13,6 +13,8 @@ export class Death {
     
     #createHitboxes() {
         //this.hitboxes[ 0 ] = new Hitbox( 0 , 0 , 0 , 0 , false )
+        this.hitboxes[ 0 ] = new Hitbox( 133.44844287297627 , -1645.8061077667423 , 937.9830621358376 , 1131.2895043283756 , false )
+        this.hitboxes[ 1 ] = new Hitbox( 1712.3099806344521 , -1571.3188340224106 , 789.998596189017 , 1185.7963336967969 , false )
     }
 
     draw(cameraX, cameraY, color = "#500000") {
