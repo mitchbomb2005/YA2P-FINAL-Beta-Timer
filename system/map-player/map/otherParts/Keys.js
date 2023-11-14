8701867df891eae23d3d74f6f9a4c1ac105540a6
loglot <prattlog3@gmail.com>
2023-11-14 13:18:35 -0600

    added more icons

--- system/map-player/map/otherParts/Keys.js
+++ system/map-player/map/otherParts/Keys.js
@@ -14,13 +14,15 @@ export class Keys {
     redFlag = new Image();
     hookDisabled = new Image();
     hookEnabled = new Image();
+    leftClick = new Image()
 
     constructor(cam){
         this.camera = cam
         this.keyD.src = 'https://raw.githubusercontent.com/loglot/key-images/main/Keyboard%20%26%20Mouse/Dark/D_Key_Dark.png';
         this.keyW.src = 'https://raw.githubusercontent.com/loglot/key-images/main/Keyboard%20%26%20Mouse/Dark/W_Key_Dark.png';
         this.keyS.src = 'https://raw.githubusercontent.com/loglot/key-images/main/Keyboard%20%26%20Mouse/Dark/S_Key_Dark.png';
-        this.keyL.src = 'https://raw.githubusercontent.com/loglot/key-images/main/Keyboard%20%26%20Mouse/Dark/L_Key_Dark.png';
+        this.keyL.src = 'https://raw.githubusercontent.com/loglot/key-images/main/Keyboard%20%26%20Mouse/Dark/L_Key_Dark.png'; //
+        this.leftClick.src = 'https://raw.githubusercontent.com/loglot/key-images/main/Keyboard%20%26%20Mouse/Dark/Mouse_Left_Key_Dark.png';
         this.keySpace.src = 'https://raw.githubusercontent.com/loglot/key-images/main/Keyboard%20%26%20Mouse/Dark/Space_Key_Dark.png'
         this.warnLava.src = 'https://raw.githubusercontent.com/loglot/yet-another-2d-platformer/main/other/images/warn.png'
         this.warnGrey.src = 'https://raw.githubusercontent.com/loglot/yet-another-2d-platformer/main/other/images/warnGrey.png'
@@ -29,11 +31,13 @@ export class Keys {
         this.hookEnabled.src = 'https://raw.githubusercontent.com/loglot/yet-another-2d-platformer/main/other/images/hookEnabled.png'
     }
     drawKeys() {
-        ctx.drawImage(this.keyD, 388 + this.camera.x, 600 + this.camera.y)
+        ctx.drawImage(this.keyD, 388 + this.camera.x, 600 + this.camera.y) //5652.9319785986945 , -181.7023653299001
         this.jump(4480, 245)
         this.jump(3850, -410)
         this.jump(2273.265700406848 , 642.4999999999999 )
         ctx.drawImage(this.hookEnabled, 5803.8752307244795 + this.camera.x, -199.17987904329323 + this.camera.y)
+        ctx.drawImage(this.redFlag, 1544.8500139551425 + this.camera.x, 67.9775041778131 + this.camera.y, 100, 100)
+        ctx.drawImage(this.leftClick, 5702.9319785986945 + this.camera.x, -181.7023653299001 + this.camera.y)
     }
 
     jump(x, y) {
