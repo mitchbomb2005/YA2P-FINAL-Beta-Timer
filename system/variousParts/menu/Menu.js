e8e80fce134f691cc860569753d7c326b0d5fd1d
loglot <prattlog3@gmail.com>
2023-12-06 14:24:13 -0600

    a better look at pause

--- system/variousParts/menu/Menu.js
+++ system/variousParts/menu/Menu.js
@@ -6,7 +6,7 @@ import { DrawUtils } from "../../../utils/DrawUtils.js"
 export class Menu {
 
     draw = new DrawUtils()
-    opacity = 1
+    opacity = 1; bgOpacity = .5
     check = false; checkDos = false
     game
 
@@ -15,7 +15,10 @@ export class Menu {
     }
 
     drawMenu() {
-        this.draw.Text("Y e t A n o t h e r 2 d P l a t f o r m e r", 300, 100, `rgba(70, 70, 70, ${this.opacity})`, `rgba(255, 255, 255, ${this.opacity})`)
+        ctx.fillStyle = `rgba(0, 0, 0, ${this.bgOpacity})`
+        ctx.rect(0, 0, 100000, 10000) 
+        ctx.fill()
+        this.draw.Text("Y e t A n o t h e r 2 d P l a t f o r m e r", 300, 150, `rgba(70, 70, 70, ${this.opacity})`, `rgba(255, 255, 255, ${this.opacity})`)
 
         this.draw.Text("P r e s s W T o S t a r t", 300, 300, `rgba(70, 70, 70, ${this.opacity})`, `rgba(255, 255, 255, ${this.opacity})`)
         this.draw.Text("P r e s s S T o E n t e r s e t t i n g s", 300, 400, `rgba(70, 70, 70, ${this.opacity})`, `rgba(255, 255, 255, ${this.opacity})`)
@@ -26,6 +29,7 @@ export class Menu {
             this.check = false
             for (let i = 0; i < 20; i++) {
                 this.opacity += .05
+                this.bgOpacity += .025
                 await this.sleep(16)
             }
             this.checkDos = false
@@ -34,6 +38,7 @@ export class Menu {
             this.checkDos = true
             for (let i = 0; i < 20; i++) {
                 this.opacity -= .05
+                this.bgOpacity -= .025
                 await this.sleep(16)
             }
             this.check = true
