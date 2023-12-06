f9815fece686c0dd46b9ee6f3eb0cb78f6d0e0a8
loglot <prattlog3@gmail.com>
2023-12-06 14:25:53 -0600

    fixed capitalisation

--- system/variousParts/menu/Menu.js
+++ system/variousParts/menu/Menu.js
@@ -21,7 +21,7 @@ export class Menu {
         this.draw.Text("Y e t A n o t h e r 2 d P l a t f o r m e r", 300, 150, `rgba(70, 70, 70, ${this.opacity})`, `rgba(255, 255, 255, ${this.opacity})`)
 
         this.draw.Text("P r e s s W T o S t a r t", 300, 300, `rgba(70, 70, 70, ${this.opacity})`, `rgba(255, 255, 255, ${this.opacity})`)
-        this.draw.Text("P r e s s S T o E n t e r s e t t i n g s", 300, 400, `rgba(70, 70, 70, ${this.opacity})`, `rgba(255, 255, 255, ${this.opacity})`)
+        this.draw.Text("P r e s s S T o E n t e r S e t t i n g s", 300, 400, `rgba(70, 70, 70, ${this.opacity})`, `rgba(255, 255, 255, ${this.opacity})`)
     }
 
     async fade(updown = "down") {
