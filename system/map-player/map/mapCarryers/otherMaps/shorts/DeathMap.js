615677d9b0c2b1fb2810e3ea49e8843d2401b18e
loglot <prattlog3@gmail.com>
2023-12-11 17:08:12 -0600

    added to map

--- system/map-player/map/mapCarryers/otherMaps/shorts/DeathMap.js
+++ system/map-player/map/mapCarryers/otherMaps/shorts/DeathMap.js
@@ -12,7 +12,9 @@ export class Death {
     }
     
     #createHitboxes() {
-        
+        this.hitboxes[0] = new Hitbox(154.8251637394651,-1082.0900028082506,315,108.9999993696722 )
+        this.hitboxes[1] = new Hitbox(455.7857448512254,-1747.08983781897,234.00226014044785,397.27747539201323 )
+        this.hitboxes[2] = new Hitbox(163.7857448512254,-2222.089969143326,221,308 )
     }
 
     draw(cameraX, cameraY, color = "#500000") {
--- system/map-player/map/mapCarryers/otherMaps/shorts/checkpoint.js
+++ system/map-player/map/mapCarryers/otherMaps/shorts/checkpoint.js
@@ -13,6 +13,9 @@ export class Checkpoint {
     
     #createHitboxes() {
         
+        this.hitboxes[0] = new Hitbox(468.14881559970235,-358.1712147705048,185.99995098218966,147.99999999967895 )
+        this.hitboxes[1] = new Hitbox(178.14881559970158,-1283.0899691403042,153.99999999998673,198.00001292616616 )
+ 
     }
 
     draw(cameraX, cameraY, color = "rgba(0, 255, 50, .3)") {
