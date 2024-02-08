73784d1913b4a56244f25b1cc43d5feedcce2a03
LogLot <prattlog3@gmail.com>
2024-02-07 19:39:46 -0600

    changed the signs a bit

--- system/map-player/map/mapCarryers/signMap.js
+++ system/map-player/map/mapCarryers/signMap.js
@@ -5,10 +5,11 @@ export class signMaker {
         this.constructSigns()
     }
     constructSigns(){ 
-        this.signs[0] = new sign(711,411,["this is a placeholder for how a sign works", "i can do multiple things with this", "", "and what can you do to stop me?", "",  "                         - the sign maker"])
-        this.signs[1] = new sign(4604 ,-5138 - 150, ["You have Successfully found a secret!", "however, this secret is not complete.", "i will continue working on it until it's done", "i hope you like what you see so far!", "", "                                - the sign maker"])
+        this.signs[0] = new sign(3821,411,["Hello There!", "This Is A Hard, But Fun Game", "If You Can't Beat Something, Try Again Until You Complete it", "", "You Can Do This",  "                                - The Sign Maker"])
+        this.signs[1] = new sign(4604 ,-5138 - 150, ["You Have Successfully Found A Secret!", "However, This Secret Is Not Complete.", "I Will Continue Working On It Until It's Done", "I Hope You Like What You See So Far!", "", "                                - The Sign Maker"])
 
-
+// this.hitboxes[this.hitboxes.length] = new Hitbox(,424,17,58 )
+ 
     }
     update(player, keyman) {
         for(let i = 0; i < this.signs.length; i++) {
